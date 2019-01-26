0930821cbec285708b6bec864af8509279c0c62c
Poly Goveia <poly.goveia@gmail.com>
2019-01-26 12:45:09 -0500

    complete first part of arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -62,7 +62,7 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -84,7 +84,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (let i = 0; i < graduates.length; i++) {
+  universities.sort().push(graduates[i].university);
+}
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -93,11 +96,21 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes('Uni')) {
+    uni.push(graduates[i].university);
+  }
+
+}
 console.log(uni);
 
 
@@ -123,6 +136,9 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(function(animal){
+  animalNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`);
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -134,7 +150,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 const lowerCase = [];
 console.log(lowerCase); 
 
-/* Request 3: .filter() 
+/* Request 3: .filter()  
 
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
